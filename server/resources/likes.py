dc5170607b1fa2717626dacb143d91e1fb2f7ef4
Mark Wainaina Wagacha <markwainaina1234@gmail.com>
2026-02-13 18:56:36 -0800

    batch like posts

--- server/resources/likes.py
+++ server/resources/likes.py
@@ -1,16 +1,26 @@
 import logging
+from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from models import Like
+from models import db, Like, User
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("likes_v8")
 
-logger = logging.getLogger("likes_v9")
- ###
-class CheckLike(Resource):
+class BatchLike(Resource):
     @jwt_required()
-    def get(self, post_id):
+    def post(self):
         user_id = int(get_jwt_identity())
-        exists = Like.query.filter_by(user_id=user_id, post_id=post_id).first()
-        logger.info(f"Checked like for user {user_id} on post {post_id}")
-        return {"liked": bool(exists)}, 200
+        data = request.get_json(silent=True) or {}
+        post_ids = data.get("post_ids", [])
+
+        created = 0
+        for pid in post_ids:
+            exists = Like.query.filter_by(user_id=user_id, post_id=pid).first()
+            if not exists:
+                db.session.add(Like(user_id=user_id, post_id=pid))
+                created += 1
+
+        db.session.commit()
+        logger.info(f"User {user_id} batch liked {created} posts")
+        return {"likes_added": created}, 201
